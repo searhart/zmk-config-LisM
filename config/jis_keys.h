cec12e37c7e55f8a1c507c6b320e18ce81047eaf
searhart <sinh0914@gmail.com>
2026-04-03 03:26:14 +0900

    Create jis_keys.h

---
+++ config/jis_keys.h
@@ -0,0 +1,13 @@
+// config/jis_keys.h
+#define J_AT    LBKT        // @
+#define J_LBKT  RBKT        // [
+#define J_RBKT  NONUS_HASH  // ]
+#define J_COLON SQT         // :
+#define J_SEMI  SEMICOLON   // ;
+#define J_HAT   EQUAL       // ^
+#define J_YEN   INT_YEN     // ￥
+#define J_UNDER INT_RO      // _
+#define J_LPAR  LS(N8)      // (
+#define J_RPAR  LS(N9)      // )
+#define J_EQUAL LS(N0)      // =
+#define J_QUOT  LS(N2)      // "
